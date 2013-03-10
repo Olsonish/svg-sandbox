cae24cd4acf49f5b3e46ffbcd6f0788460b9a008
ivan <olsonish@gmail.com>
2013-03-10 15:48:30 -0700

    pushing and deploy to nodejitsu for review

--- app/public/javascripts/views/home.js
+++ app/public/javascripts/views/home.js
@@ -1,5 +1,4 @@
 d3.json('/data/data.json', function (data) {
-    d = data;
 
     /** Define Sandbox */
     svg = d3.select('#sb-master').append('svg');
@@ -29,22 +28,21 @@ d3.json('/data/data.json', function (data) {
         });
 
     /** Basic Line Graph Tutorial Start */
-    var margin = { top: 20, right: 20, bottom: 30, left: 50 },
+    var margin = { top: 20, right: 20, bottom: 20, left: 20 },
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
-    var parseDate = d3.time.format('%d-%b-%y').parse;
+    var parseDate = d3.time.format('%m-%y').parse;
 
     var x = d3.time.scale().range([0, width]);
-
     var y = d3.scale.linear().range([height, 0]);
 
     var xAxis = d3.svg.axis().scale(x).orient('bottom');
-
     var yAxis = d3.svg.axis().scale(y).orient('left');
 
     var line = d3.svg.line()
-        .x(function (d) { return x(d.date); });
+        .x(function (d) { return x(d.date); })
+        .y(function (d) { return y(d.close); });
 
     svg.attr('width', width + margin.left + margin.right)
         .attr('height', height + margin.top + margin.bottom)
@@ -52,7 +50,10 @@ d3.json('/data/data.json', function (data) {
             .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
     /** Make Line Graph */
-    d3.tsv('/data/data.tsv', function (err, data) {
+
+    d3.json('/data/date-close.json', function (err, data) {
+        d = data;
+
         data.forEach(function (d) {
             d.date = parseDate(d.date);
             d.close = +d.close;
@@ -61,10 +62,13 @@ d3.json('/data/data.json', function (data) {
         x.domain(d3.extent(data, function (d) { return d.date; }));
         y.domain(d3.extent(data, function (d) { return d.close; }));
 
+        console.log(d3.extent(data, function (d) { return d.close; }));
+
         svg.append('g')
             .attr('class', 'x axis')
             .attr('transform', 'translate(0,' + height + ')')
             .call(xAxis);
+
 
         svg.append('g')
             .attr('class', 'y axis')
@@ -76,7 +80,11 @@ d3.json('/data/data.json', function (data) {
             .style('text-anchor', 'end')
             .text('price ($)');
 
+        svg.append('path')
+            .datum(data)
+            .attr('class', 'line')
+            .attr('d', line);
+
 
     });
-
 });
