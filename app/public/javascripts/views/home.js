dce99725bb8f2afa1ddf8e67bd2d2f3380725460
ivan <olsonish@gmail.com>
2013-03-09 22:16:01 -0800

    hover effect returning json data, time to set up a graph

--- app/public/javascripts/views/home.js
+++ app/public/javascripts/views/home.js
@@ -1,24 +1,32 @@
 d3.json('/data/data.json', function (data) {
+    d = data;
 
     /** Define it */
-    svg = d3.select('#sb-master');
-    
+    svg = d3.select('#sb-master').append('svg');
+    var nodes = svg.append('g').attr('class', 'nodes');
+
     /** Make it */
-    svg.append('svg')
-        .selectAll('circle')
+    nodes.selectAll('circle')
         .data(data)
         .enter()
-        .append('circle')
+        .append('svg:circle')
         .attr('cx', function (d, i) { return 20; })
         .attr('cy', function (d, i) { return (i * 25) + 25; })
-        .attr('r', 9);
-
-    /** Then do stuff with it */
-    svg.selectAll('circle')
+        .attr('r', 9)
         .on('mouseover', function () {
-            d3.select(this).classed('active', true);
+            d3.select(this)
+                .classed('active', true)
+                .attr('r', 12)
+                .append('svg:title')
+                .text(function(d){return d.count;});
         })
         .on('mouseout', function () {
-            d3.select(this).classed('active', false);
+            d3.select(this)
+                .classed('active', false)
+                .attr('r', 9);
+            d3.select(this.parentNode).select('.tip')
+                .remove();
         });
+
+
 });
