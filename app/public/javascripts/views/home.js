2be1b042152d0317985242aab00e3d3f319bbb79
ivan <olsonish@gmail.com>
2013-03-10 13:37:07 -0700

    added new data, rickshaw lib, media-queries less, and pushed to nodejitsu

--- app/public/javascripts/views/home.js
+++ app/public/javascripts/views/home.js
@@ -1,16 +1,16 @@
 d3.json('/data/data.json', function (data) {
     d = data;
 
-    /** Define it */
+    /** Define Sandbox */
     svg = d3.select('#sb-master').append('svg');
     var nodes = svg.append('g').attr('class', 'nodes');
 
-    /** Make it */
+    /** Make Sandbox */
     nodes.selectAll('circle')
         .data(data)
         .enter()
         .append('svg:circle')
-        .attr('cx', function (d, i) { return 20; })
+        .attr('cx', function (d, i) { return 900; })
         .attr('cy', function (d, i) { return (i * 25) + 25; })
         .attr('r', 9)
         .on('mouseover', function () {
@@ -18,7 +18,7 @@ d3.json('/data/data.json', function (data) {
                 .classed('active', true)
                 .attr('r', 12)
                 .append('svg:title')
-                .text(function(d){return d.count;});
+                .text(function (d) { return d.count; });
         })
         .on('mouseout', function () {
             d3.select(this)
@@ -28,5 +28,55 @@ d3.json('/data/data.json', function (data) {
                 .remove();
         });
 
+    /** Basic Line Graph Tutorial Start */
+    var margin = { top: 20, right: 20, bottom: 30, left: 50 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var parseDate = d3.time.format('%d-%b-%y').parse;
+
+    var x = d3.time.scale().range([0, width]);
+
+    var y = d3.scale.linear().range([height, 0]);
+
+    var xAxis = d3.svg.axis().scale(x).orient('bottom');
+
+    var yAxis = d3.svg.axis().scale(y).orient('left');
+
+    var line = d3.svg.line()
+        .x(function (d) { return x(d.date); });
+
+    svg.attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    /** Make Line Graph */
+    d3.tsv('/data/data.tsv', function (err, data) {
+        data.forEach(function (d) {
+            d.date = parseDate(d.date);
+            d.close = +d.close;
+        });
+
+        x.domain(d3.extent(data, function (d) { return d.date; }));
+        y.domain(d3.extent(data, function (d) { return d.close; }));
+
+        svg.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0,' + height + ')')
+            .call(xAxis);
+
+        svg.append('g')
+            .attr('class', 'y axis')
+            .call(yAxis)
+          .append('text')
+            .attr('transform', 'rotate(-90)')
+            .attr('y', 6)
+            .attr('dy', '.71em')
+            .style('text-anchor', 'end')
+            .text('price ($)');
+
+
+    });
 
 });
